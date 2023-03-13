9355e891354f165fcc25db0b7ac74893518ec2e9
thens18 <thensthennavan3@ggmail.com>
2023-03-13 21:01:12 +0530

    array created

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pyfiglet as pyg
+import numpy as np
 
-res= pyg.figlet_format("Welcome to Terminal Battleground ")
+res = pyg.figlet_format("Welcome to Terminal Battleground ")
 print(res)
 
-name=input("Enter your name :")
-country=input("Enter your name :")
-repo=[name]
+name = input("Enter your name :")
+country = input("Enter your name :")
+repo = [name]
+comm = np.array(["cd", "pwd", "mkdir", "proceed"])
 while True:
-    a=input(name+"@"+country +":" + repo[0]+" $")
-    if(a=="quit"):
+    a = input(name + "@" + country + ":" + repo[0] + " $")
+    if a == "quit":
         exit()
+    elif (a == comm).any():
+        print("the given command is present")
