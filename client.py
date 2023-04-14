2c26da7171308831a72201a948da538d27cd0a79
thens18 <thensthennavan3@ggmail.com>
2023-04-14 21:01:08 +0530

    new update

--- client.py
+++ client.py
@@ -1,25 +1,16 @@
 import socket
-import threading
-
-def receive_message(client_socket):
-    while True:
-        message = client_socket.recv(1024)
-        print(message.decode())
-
-# create a socket object
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# get the server's IP address and port
-server_address = ('192.168.192.1', 9999)  # replace with the actual IP address and port
-
-# connect to the server
-client_socket.connect(server_address)
-
-# start a thread to receive messages from the server
-receive_thread = threading.Thread(target=receive_message, args=(client_socket,))
-receive_thread.start()
-
-# send messages to the server
+ClientMultiSocket = socket.socket()
+host = '127.0.0.1'
+port = 2004
+print('Waiting for connection response')
+try:
+    ClientMultiSocket.connect((host, port))
+except socket.error as e:
+    print(str(e))
+res = ClientMultiSocket.recv(1024)
 while True:
-    message = input()
-    client_socket.send(message.encode())
+    Input = input('Hey there: ')
+    ClientMultiSocket.send(str.encode(Input))
+    res = ClientMultiSocket.recv(1024)
+    print(res.decode('utf-8'))
+ClientMultiSocket.close()
--- server.py
+++ server.py
@@ -1,38 +1,29 @@
 import socket
-
-# create a socket object
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# get the hostname and IP address of the machine
-hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
-print('Server IP address:', ip_address)
-
-# set the IP address and port to listen on
-ip_port = (ip_address, 9999)
-
-# bind the socket to the IP address and port
-server_socket.bind(ip_port)
-
-# listen for incoming connections
-server_socket.listen(5)
-print('Waiting for client connections...')
-
-# accept incoming connections
-client_sockets = []
-while len(client_sockets) < 2:
-    client_socket, address = server_socket.accept()
-    print(f'Connection from {address}')
-    client_sockets.append(client_socket)
-
-# chat between the clients
+import os
+from _thread import *
+ServerSideSocket = socket.socket()
+host = '127.0.0.1'
+port = 2004
+ThreadCount = 0
+try:
+    ServerSideSocket.bind((host, port))
+except socket.error as e:
+    print(str(e))
+print('Socket is listening..')
+ServerSideSocket.listen(5)
+def multi_threaded_client(connection):
+    connection.send(str.encode('Server is working:'))
+    while True:
+        data = connection.recv(2048)
+        response = 'Server message: ' + data.decode('utf-8')
+        if not data:
+            break
+        connection.sendall(str.encode(response))
+    connection.close()
 while True:
-    # receive message from client 1 and send it to client 2
-    message = client_sockets[0].recv(1024)
-    if message:
-        client_sockets[1].send(message)
-
-    # receive message from client 2 and send it to client 1
-    message = client_sockets[1].recv(1024)
-    if message:
-        client_sockets[0].send(message)
+    Client, address = ServerSideSocket.accept()
+    print('Connected to: ' + address[0] + ':' + str(address[1]))
+    start_new_thread(multi_threaded_client, (Client, ))
+    ThreadCount += 1
+    print('Thread Number: ' + str(ThreadCount))
+ServerSideSocket.close()
